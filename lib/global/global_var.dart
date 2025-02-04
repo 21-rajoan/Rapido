1f67873af9e5180f5333a788e70793a9f303f2cc
Rajoan Tamjid <rajoantamjid.21@gmail.com>
2025-02-04 17:33:34 +0600

    google map intriget & user app complete

--- lib/global/global_var.dart
+++ lib/global/global_var.dart
@@ -1,7 +1,7 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 String userName = "";
-String googleMapKey = "AIzaSyCaHdJVg_TDRArSohhLcKZXtmZV33AEgUE";
+String googleMapKey = "AIzaSyBxUHiDpTx9k1m7BipbI102zAn2cnClJN8";
 const CameraPosition googlePlexInitialPosition = CameraPosition(
   target: LatLng(37.42796133580664, -122.085749655962),
   zoom: 14.4746,
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:user_app/global/global_var.dart';
 
@@ -14,8 +18,38 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final Completer<GoogleMapController> googleMapCompleaterController =
       Completer<GoogleMapController>();
-
   GoogleMapController? controllerGoogleMap;
+  Position? currentPositionOfUser;
+
+  void updateMapThemes(GoogleMapController controller) {
+    getJsonFileFromThemes("themes/retro_style.json")
+        .then((value) => setGoogleMapStyle(value, controller));
+  }
+
+  Future<String> getJsonFileFromThemes(String mapStylePath) async {
+    ByteData byteData = await rootBundle.load(mapStylePath);
+    var list = byteData.buffer
+        .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes);
+    return utf8.decode(list);
+  }
+
+  setGoogleMapStyle(String googleMapStyle, GoogleMapController controller) {
+    controller.setMapStyle(googleMapStyle);
+  }
+
+  getCurrentLiveLocationOfUser() async {
+    Position positionOfUser = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.bestForNavigation);
+    currentPositionOfUser = positionOfUser;
+
+    LatLng positionOfUserInLatLng = LatLng(
+        currentPositionOfUser!.latitude, currentPositionOfUser!.longitude);
+
+    CameraPosition cameraPosition =
+        CameraPosition(target: positionOfUserInLatLng, zoom: 15);
+    controllerGoogleMap!
+        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +62,9 @@ class _HomePageState extends State<HomePage> {
           initialCameraPosition: googlePlexInitialPosition,
           onMapCreated: (GoogleMapController mapController) {
             controllerGoogleMap = mapController;
+            updateMapThemes(controllerGoogleMap!);
             googleMapCompleaterController.complete(controllerGoogleMap);
+            getCurrentLiveLocationOfUser();
           },
         ),
       ],
